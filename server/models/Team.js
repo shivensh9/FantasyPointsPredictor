1e55dd333c1f85f1295472274a93c986c60b4b0c
Shiven Sharma <shivii2526@MacBook-Pro.local>
2023-08-19 13:57:40 +0530

    Created schema for team

--- server/models/Team.js
+++ server/models/Team.js
@@ -1,2 +1,16 @@
 import mongoose from "mongoose";
-export const teamSchema = new mongoose.Schema({});
+export const teamSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+    },
+    team: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const TeamModel = new mongoose.model("team", teamSchema);
+export default TeamModel;
